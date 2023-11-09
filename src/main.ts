992b0364a1e6b3a81e1416e2148b3d00f78c54e9
chrisais9 <chrisais9@naver.com>
2023-11-09 15:45:42 +0900

    (fix) prettier rule

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
+} from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
