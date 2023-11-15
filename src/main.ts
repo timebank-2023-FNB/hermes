1cb0f3dd7da4dbaf954b974736ea0d0c3fb76e96
Pilmo <pilmokim99@gmail.com>
2023-11-15 23:02:43 +0900

    (add) swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
@@ -10,6 +11,16 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle("Example API")
+    .setDescription("The example API description")
+    .setVersion("1.0")
+    .addTag("example")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
 }
 
