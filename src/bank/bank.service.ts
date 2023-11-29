425d6291e43479fffb5f0316297f5fa87e3b1827
chrisais9 <chrisais9@naver.com>
2023-11-29 21:24:52 +0900

    (add) apis

--- src/bank/bank.service.ts
+++ src/bank/bank.service.ts
@@ -1,6 +1,33 @@
 import { Injectable } from "@nestjs/common";
+import TransactionDto from "./dto/transaction.dto";
+import axios from "axios";
+import { API_BANK } from "src/config";
 
 @Injectable()
 export class BankService {
   private constructor() {}
+
+  async registerPayApp() {
+    const response = await axios.post(`${API_BANK}/mock/pay/register`, {});
+    console.log(response);
+    return "123123";
+  }
+
+  async getBalance(payId: string, bankAccount: string) {
+    const response = await axios.post(`${API_BANK}/mock/balance`, {
+      payId,
+      bankAccount,
+    });
+    console.log(response);
+    return 10000;
+  }
+
+  async transfer(payId: string, transaction: TransactionDto) {
+    const response = await axios.post(`${API_BANK}/mock/balance`, {
+      ...transaction,
+      payId,
+    });
+    console.log(response);
+    return true;
+  }
 }
