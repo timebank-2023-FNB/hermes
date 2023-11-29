4efe8940f40002ba7799cadd4448ef25d21c5316
chrisais9 <chrisais9@naver.com>
2023-11-29 22:15:27 +0900

    (add) docker-compose and dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,12 @@ import { HmacGuard } from "./guards/hmac.guard";
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>("MONGODB_URI"),
+        uri: configService.get<string>("DATABASE_URI"),
+        dbName: configService.get<string>("DATABASE_NAME"),
+        auth: {
+          username: configService.get<string>("DATABASE_USER"),
+          password: configService.get<string>("DATABASE_PASS"),
+        },
       }),
       inject: [ConfigService],
     }),
