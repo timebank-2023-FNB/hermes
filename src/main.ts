29dc34cbd45b6f227ef184f3ee5263166639b846
chrisais9 <chrisais9@naver.com>
2023-11-30 11:48:43 +0900

    (fix) dev HOST

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
-  await app.listen(3000, "0.0.0.0");
+  await app.listen(3000, process.env.HOST || "0.0.0.0");
 }
 
 bootstrap();
